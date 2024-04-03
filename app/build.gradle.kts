1867f7f9e52b244f7cb7278ea327d6059c621e8b
Fabi019 <fabi019@gmx.de>
2024-04-03 20:33:27 +0200

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.fabik.merossble"
         minSdk = 23
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
